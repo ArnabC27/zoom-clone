e1115932d6fccc6f6f7a39207273e25cb595eab4
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-04-02 23:53:39 +0530

    02.04.2024

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,7 +14,14 @@ const Sidebar = () => {
           const isActive = pathname === link.route || pathname.startsWith(link.route);
 
           return (
-            <Link href={link.route} key={link.label} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start')}>
+            <Link 
+              href={link.route} 
+              key={link.label} 
+              className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
+                'bg-blue-1': isActive,
+              })}
+            >
+              
             </Link>
           )
         })}
