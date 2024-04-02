d01226636bad7ae8af675dec0b2b5432787632fc
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-04-02 23:01:54 +0530

    02.04.2024

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Meeting = ({ params }: { params: { slug: string } }) => {
+const Meeting = ({ params }: { params: { id: string } }) => {
   return (
-    <div>Meeting</div>
+    <div>Meeting Room: #{params.id}</div>
   )
 }
 
